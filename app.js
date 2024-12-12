3685da856542c07595c34125e21d644811206d51
dilmurodivcc <zufarovamohire32.gmail.com>
2024-12-13 00:29:03 +0500

    Done Successfuly

--- app.js
+++ app.js
@@ -1,118 +1,100 @@
-function Triangle() {
-  const side = +document.getElementById("inputValue").value;
-  const p = 3 * side;
-  const s = (1.732 / 4) * (side * side);
-  document.getElementById("output").innerHTML = `
-      Perimetr: ${p}sm <br>
-      Yuza: ${s.toFixed(2)}sm^2
-  `;
-}
-
-
-
-
-
-function InvertDigit() {
-  const a = +document.getElementById("numA").value;
-  const b = +document.getElementById("numB").value;
-  const c = +document.getElementById("numC").value;
-  const reverse = (n) => {
-    return parseInt(n.toString().split("").reverse().join(""));
-  }
-  document.getElementById(
-    "output2"
-  ).innerHTML = `Teskari raqamla: <br> a = ${reverse(a)}, b = ${reverse(b)}, c = ${reverse(c)}`;
-}
-
-
-
-
-
-
-function DigitCountSum() {
-  const a = +document.getElementById("A").value;
-  const b = +document.getElementById("B").value;
-  const c = +document.getElementById("C").value;
-  const count = (n) => {
-    return n.toString().length;
-  };
-  const calculateSum = (n) => {
-    const str = n.toString();
-    let sum = 0;
-    for (let i = 0; i < str.length; i++) {
-      sum += parseInt(str[i]);
+function m1() {
+  const input = document.getElementById("inputValue").value;
+  const array = input.trim().split(" ").map(Number);
+  for (let i = 0; i < array.length; i++) {
+    let count = 0;
+    for (let c = 0; c < array.length; c++) {
+      if (array[i] === array[c]) {
+        count++;
+      }
     }
-    return sum;
-  };
+    if (count == 1) {
+      return (document.getElementById(
+        "output"
+      ).innerHTML = `Takrorlanmas element:  ${array[i]} <br> index = ${i}`);
+    }
+  }
+  return (document.getElementById(
+    "output"
+  ).innerHTML = `Takrorlanmas element yoq.`);
+}
+function m2() {
+  const input = document.getElementById("inputValue2").value;
+  const array = input.trim().split(" ").map(Number);
+  const result = array.map((num) => num * -1).join(" ");
+  document.getElementById("output2").innerHTML = `O'zgargan arr: ${result}`;
+}
+function m3() {
+  const input = document.getElementById("inputValue3").value;
+  const array = input.trim().split(" ").map(Number);
+  const result = array.map((num) => num ** 2).join(" ");
   document.getElementById(
     "output3"
-  ).innerHTML = `A soni: Raqamlar soni - ${count(
-    a
-  )}, <br> Raqamlar yig'indisi - ${calculateSum(a)}.<br> 
-  B soni: Raqamlar soni - ${count(
-    b
-  )}, <br> Raqamlar yig'indisi - ${calculateSum(b)}.<br> 
-  C soni: Raqamlar soni - ${count(
-    c
-  )}, <br> Raqamlar yig'indisi - ${calculateSum(c)}.`;
+  ).innerHTML = `Kvadratdga oshirilgan arr:<br> ${result}`;
 }
-function m5() {
-  const num1 = +document.getElementById("num1").value;
-  const num2 = +document.getElementById("num2").value;
-  const num3 = +document.getElementById("num3").value;
-  const even = (K) => {
-    if (K % 2 === 0) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  document.getElementById("output5").innerHTML = `1 - son: ${even(num1)} <br>
-                 2 - son: ${even(num2)} <br>
-                 3 - son: ${even(num3)}`;
-}
-function IsPowerN() {
-  const n = +document.getElementById("n").value;
-  const k1 = +document.getElementById("k1").value;
-  const k2 = +document.getElementById("k2").value;
-  const k3 = +document.getElementById("k3").value;
-  const k4 = +document.getElementById("k4").value;
-  const k5 = +document.getElementById("k5").value;
-  let result = 0;
-  const check = (k, n) => {
-    while (k > 1) {
-      if (k % n !== 0) {
-        return false;
+function m4() {
+  const input = document.getElementById("inputValue4").value;
+  const array = input.trim().split(" ").map(Number);
+  for (let i = 0; i < array.length; i++) {
+    let count = 0;
+    for (let c = 0; c < array.length; c++) {
+      if (array[i] === array[c]) {
+        count++;
       }
-      k = k / n;
     }
-    return k === 1;
-  };
-  const K = [k1, k2, k3, k4, k5];
-  for (let i = 0; i < K.length; i++) {
-    if (check(K[i], n)) {
-      result++;
+    if (count != 1) {
+      array.splice(i, 1);
+      i--;
     }
   }
-  document.getElementById(
-    "output6"
-  ).innerHTML = `Ning darajasi bo'lgan sonlar soni: ${result}`;
+  return (document.getElementById(
+    "output4"
+  ).innerHTML = `Takrorlanmas elementlar o'chirildi: <br>  [ ${array} ]`);
+}
+function m5() {
+  const n = +document.getElementById("n").value;
+  const input = document.getElementById("inputValue5").value;
+  const array = input.trim().split(" ").map(Number);
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % n !== 0) {
+      count++;
+    }
+  }
+  return (document.getElementById(
+    "output5"
+  ).innerHTML = `Arrey da ${n} ga bo'linmiydigan element  ${count} ta.`);
 }
 
-function IncTime() {
-  const h = +document.getElementById("H").value;
-  const m = +document.getElementById("M").value;
-  const s = +document.getElementById("S").value;
-  const t = +document.getElementById("T").value;
-  let totalS = s + t;
-  let tS = totalS % 60;
-  let totalM = m + Math.floor(totalS / 60);
-  let tM = totalM % 60;
-  let tH = h + Math.floor(totalM / 60);
-  document.getElementById("output7").innerHTML = `
-  ${t} sekund qo'shilganidan keyin: <br>
-  ${tH} soat ${tM} minut ${tS} sekund.`;
+function m6() {
+  const input = document.getElementById("inputValue6").value;
+  const array = input.trim().split(" ").map(Number);
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+    if (el % 2 === 0) {
+      document.getElementById(
+        "output6"
+      ).innerHTML += `  index = ${i}  element = [ ${el} ]<br>`;
+    }
+  }
 }
+function m7() {
+  const input = document.getElementById("inputValue7").value;
+  const array = input.trim().split(" ").map(Number);
+  array.sort((a, b) => a - b);
+  document.getElementById("output7").innerHTML = `Eng katta element: [ ${
+    array[array.length - 1]
+  } ]`;
+}
+
+function m8() {
+  const input = document.getElementById("inputValue8").value;
+  const array = input.trim().split(" ").map(Number).reverse().join(", ");
+  document.getElementById(
+    "output8"
+  ).innerHTML = `Teskari tertibda: [ ${array} ]`;
+}
+
 // SOME FUNCTION -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 const moonIcon = document.querySelector(".moon-icon");
@@ -182,7 +164,21 @@ document.addEventListener("mousemove", (e) => {
     smoke.remove();
   }, 550);
 });
-AOS.init({
-  duration: 1700,
-  once: true,
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
+
+
